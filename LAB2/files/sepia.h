f4032440d52e3e66f8c5c110fc45a59d2a337b39
Conor Hickey <conor.hickey1@gmail.com>
2016-02-22 12:31:47 +0000

    HELP

--- LAB2/files/sepia.h
+++ LAB2/files/sepia.h
@@ -1,6 +1,6 @@
 #ifndef SEPIA_H
 #define SEPIA_H
-extern "C"
+
 void sepia(struct image *im, struct image *sim);
 
 #endif  /* SEPIA_H */
--- LAB2/files/sepia_par.cu
+++ LAB2/files/sepia_par.cu
@@ -1,4 +1,6 @@
-
+extern "C"{
+#include "sepia.h"
+}
 #include "image.h"
 #include "sepia.h"
 #include <stdlib.h>
@@ -9,12 +11,15 @@
 /*   r:      number of rows */
 /*   c:      number of cols */
 __global__ void sepia_ker(struct pixel *inbuf, struct pixel *outbuf, int r, int c)
-{
+{	
 	int i;
+
 	uint32_t temp;
-	i = blockIdx.x * blockDim.x * threadIdx.x;
-	for(i=0;i<r*c;i++){
-	//if(i<r*c){    	
+	i = (blockIdx.x * blockDim.x) * threadIdx.x;
+	for( ; ; ) {
+      int x = 0;
+	} 
+	if(i<r*c){ 
 		temp = 0.393f * inbuf[i].r + 0.769f * inbuf[i].g + 0.189f * inbuf[i].b;    	
 	   	if(temp > 65535){
 	   	outbuf[i].r = 65535;
@@ -40,6 +45,8 @@ __global__ void sepia_ker(struct pixel *inbuf, struct pixel *outbuf, int r, int 
 		}
 		
 		outbuf[i].a = inbuf[i].a;
+
+		
 		
      }
 }
@@ -53,19 +60,26 @@ void sepia(struct image *im, struct image *sim)
     sim->cols= im->cols;
     struct pixel *dpixSimP,*dpixP;
     struct pixel *pixSimP,*pixP;
+    printf("%d rows\n",im->rows);
+    printf("%d cols \n",im->cols);
+    
+
     int n = sim->rows * sim->cols;
+    int size = n*sizeof(struct pixel);
     int n_threads = 1024;
-    int n_blocks = (n-1)/1024 +1;
+    int n_blocks = (n-1)/(n_threads +1);
 	printf("%d\n",n_blocks);
-    sim->pixbuf = (struct pixel *)malloc(n*sizeof(struct pixel));
+    sim->pixbuf = (struct pixel *)malloc(size);
     pixP = im->pixbuf;
 	pixSimP = sim->pixbuf;
-	cudaMalloc((void **) &dpixSimP,n*sizeof(struct pixel));
-	cudaMalloc((void **) &dpixP,n*sizeof(struct pixel));
+	cudaMalloc((void **) &dpixSimP,size);
+	cudaMalloc((void **) &dpixP,size);
 	
-	cudaMemcpy(dpixSimP,pixSimP, n*sizeof(struct pixel),cudaMemcpyHostToDevice);
-	cudaMemcpy(&dpixP, pixP, n*sizeof(struct pixel),cudaMemcpyHostToDevice);
-	sepia_ker<<<n_blocks,n_threads>>>(sim->pixbuf,im->pixbuf,im->rows,im->cols);
+	cudaMemcpy(dpixP,im->pixbuf,size,cudaMemcpyHostToDevice);
+	//sepia_ker<<<n_blocks,n_threads>>>(im->pixbuf,sim->pixbuf,im->rows,im->cols);
+	sepia_ker<<<30,30>>>(im->pixbuf,sim->pixbuf,im->rows,im->cols);
+	
+	cudaMemcpy(sim->pixbuf, dpixSimP, size,cudaMemcpyDeviceToHost);
 	
 	
 	cudaFree(dpixSimP);
